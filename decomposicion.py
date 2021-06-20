cec8e109f56af10b6908848cb1c29f9d044f18b6
Ian <sebastianvaz123@hotmail.com>
2021-06-20 01:49:56 -0500

    Mas clases creadas

--- decomposicion.py
+++ decomposicion.py
@@ -1,7 +1,20 @@
 class Automovil:
-    def __init__(self,modelo,marca,color):
-        self.modelo=modelo
-        self.marca=marca
-        self.color=color
+    def __init__(self, modelo, marca, color):
+        self.modelo = modelo
+        self.marca = marca
+        self.color = color
+        self._estado = 'en_reposo'
+        self._motor = None
 
-        
+
+class Motor:
+
+    def __init__(self, cilindros, tipo='gasolina'):
+        self.cilindros = cilindros
+        self.tipo = tipo
+        self._temperatura = 0
+
+    def inyecta_gasolina(self, cantidad):
+        pass
+
+

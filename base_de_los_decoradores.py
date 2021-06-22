5e7f5062aa0992d76a79458e799ef351c07ba930
Ian <sebastianvaz123@hotmail.com>
2021-06-21 23:23:11 -0500

    Practica de Setter && Getters

--- base_de_los_decoradores.py
+++ base_de_los_decoradores.py
@@ -4,26 +4,31 @@ def potencia_cubo(numero):
     """
     return numero**3
 
+
 def presentarse(nombre):
     return f'Me llamo {nombre}'
+
 
 def estudiemos_juntos(nombre):
     return f'Hey estudiemos juntos python {nombre}'
 
+
 def consume_funciones(funcion_a_llamar):
     return funcion_a_llamar("Sebastian")
 
+
 def funcion_mayor():
-	print("Esta es una función mayor y su mensaje de salida.")
+    print("Esta es una función mayor y su mensaje de salida.")
 
-	def librerias():
-		print("Algunas librerías de Python son: Scikit-learn, NumPy y TensorFlow.")
+    def librerias():
+        print("Algunas librerías de Python son: Scikit-learn, NumPy y TensorFlow.")
 
-	def frameworks():
-		print("Algunos frameworks de Python son: Django, Dash y Flask.")
+    def frameworks():
+        print("Algunos frameworks de Python son: Django, Dash y Flask.")
 
-	frameworks()
-	librerias()
+    frameworks()
+    librerias()
+
 
 if __name__ == "__main__":
     print(consume_funciones(presentarse))

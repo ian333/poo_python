abf0bca277bfc2d828353d964a344c0e3b278324
Ian <sebastianvaz123@hotmail.com>
2021-06-21 22:34:12 -0500

    Haciendo ejemplos de abstraccion

---
+++ abstraccion.py
@@ -0,0 +1,24 @@
+class Lavadora:
+    """
+    docstring
+    """
+    def __init__(self):
+        pass
+    
+    def lavar (self,temperatura="Caliente"):
+        self._llenar_tanque_de_agua(temperatura)
+        self._anadir_jabon()
+        self._lavar()
+        self._centrifugar()
+    def _llenar_tanque_de_agua(self,temperatura):
+        """
+        docstring
+        """
+        print(f'Llenando el tanque con agua en {temperatura}')
+    def _anadir_jabon(self):
+        """
+        docstring
+        """
+        print("Añadiendo Jabon")
+    def _lavar(self):
+        print ("Lavando la ropa")

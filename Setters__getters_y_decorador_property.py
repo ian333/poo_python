13f18a3425188576678a6bc929ba7be5156d9547
Ian <sebastianvaz123@hotmail.com>
2021-06-24 22:39:09 -0500

    En este commit se termino el programa de setter y getter con property
    Signed-off-by: Ian <sebastianvaz123@hotmail.com>

--- Setters__getters_y_decorador_property.py
+++ Setters__getters_y_decorador_property.py
@@ -20,7 +20,12 @@ class Millas():
     """
     Esta clase convierte los kilometros a millas
     """
-
+    def Conversion_a_kilometros(self):
+        """
+        Esta funcion convierte los kilometros a millas 
+        """
+        return(self._distancia*1.609344)
+    
     def __init__(self):
         """
         En la funcion __init__ es el constructor y aqui se definen las variables de la clase con el estilo
@@ -28,35 +33,36 @@ class Millas():
         """
         self._distancia = 0
 
-    def Conversion_a_kilometros(self):
-        """
-        Esta funcion convierte los kilometros a millas 
-        """
-        return(self._distancia*1.609344)
-
     # Metodo getter
-    def obtener_distancia(self):
+    @property
+    def distancia(self):
+        """
+        asignamos la funcion distancia
+        """
+        pass
+    @distancia.getter
+    def distancia(self):
         """
         Esta funcion devuelve la distancia
         """
         print("Llamada del metodo getter")
         return self._distancia
-
-    def definir_distancia(self, recorrido):
+    @distancia.setter    
+    def distancia(self, recorrido):
         """
         funcion para definir el valor de _distancia 
         """
         print(f"Llamada al metodo Setter y el valor de recorrido es {recorrido}")
         self._distancia = recorrido
-
-    def eliminar_distancia(self):
+    @distancia.deleter  
+    def distancia(self):
         """
         Funcion para eliminar el atributo _distancia 
         """
         del self._distancia
         
 
-    distancia = property(obtener_distancia, definir_distancia, eliminar_distancia)
+    #distancia = property(obtener_distancia, definir_distancia, eliminar_distancia)
 
 
 if __name__ == "__main__":
@@ -64,3 +70,5 @@ if __name__ == "__main__":
     avion = Millas()
     avion.distancia = 200
     print(avion.distancia)
+    print(avion.Conversion_a_kilometros())
+

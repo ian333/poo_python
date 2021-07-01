48f12156e02d258da1531d4e559723b44ae39850
Ian <sebastianvaz123@hotmail.com>
2021-06-30 20:32:26 -0500

    Este commit es para nuestro programa de complejidad algoritmica
    Signed-off-by: Ian <sebastianvaz123@hotmail.com>

---
+++ complejidad_algoritmica.py
@@ -0,0 +1,35 @@
+import time
+
+def factorial(n):
+    """
+    Esta funcion es una prueba de complejidad algoritmica
+    """
+    respuesta = 1
+
+    while n >1:
+        respuesta *=n
+        n-=1
+    return respuesta
+
+def factorial_r(n):
+    """
+    Esta funcion es una prueba de complejidad algoritmica
+    """
+    if n==1:
+        return 1
+    else:
+        return n * factorial_r(n-1)
+
+if __name__ == "__main__":
+    n=1000
+    comienzo= time.time()
+    factorial(n)
+    final=time.time()
+    print("Esta es la funcion no recursiva")
+    print(final-comienzo)
+
+    comienzo= time.time()
+    factorial_r(n)
+    final=time.time()
+    print("Esta es la funcion no recursiva")
+    print(final-comienzo)
